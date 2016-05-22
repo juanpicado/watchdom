b8e40155f22eeee2359c46235995bc106ed03897
Juan Picado (@jotadeveloper) <juanpicado19@gmail.com>
2016-05-22 23:52:16 +0200

    Added unit test

---
+++ test/test.js
@@ -0,0 +1,36 @@
+/* globals document, describe, domWatcher, chai */
+var expect = (chai && chai.expect) || require('chai').expect;
+
+describe('domWatcher Tests', function() {
+
+    var params = {
+        childList: true,
+        attributes: true,
+        characterData: true,
+        subtree: true,
+        attributeOldValue: true,
+        characterDataOldValue: true
+    };
+
+    before(function() {
+        var example = document.createElement('div');
+        var body = document.getElementsByTagName('body');
+        example.id = 'example';
+        body[0].appendChild(example);
+    });
+
+    it('mutableObserver', function(done) {
+            var myElement = document.getElementById('example');
+            var watcher = domWatcher();
+            watcher.observe(myElement, params);
+            watcher.notify(function(mutation) {
+                expect(mutation.type).to.equal('childList');
+                watcher.stop();
+                done();
+            });
+            var myNewElement = document.createElement('div');
+            myNewElement.innerHTML = 'test';
+            myElement.appendChild(myNewElement);
+        });
+
+});
