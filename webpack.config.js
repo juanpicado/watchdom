afe82a356ac408c54657efc2ecf7c8d56ab0bd68
Juan Picado (@jotadeveloper) <juanpicado19@gmail.com>
2016-05-28 23:08:14 +0200

    Fixed using module as npm dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,4 @@
-const webpack = require( "webpack" );
-
-module.exports = {
-    entry: "./lib/index.js",
-    output: {
-        path: "dist/",
-        filename: "index.js",
-
-        // Export itself to a global var
-        libraryTarget: "var",
-
-        // Name of the global var: "Foo"
-        library: "domWatcher"
-    },
-
-    // Module: {
-    //     loaders: [{
-    //         test: /\.jsx?$/,
-    //         exclude: /node_modules/,
-    //         loader: 'babel',
-    //     }]
-    // },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin( {
-            compress: {
-                warnings: false
-            },
-            output: {
-                comments: false
-            }
-        } )
-    ]
-};
+var DefaultConfig = require('webpack-babel-conf');
+var mutationConf = DefaultConfig('./lib/browser.js', './watchdom.browser.min.js', 'domWatcher');
+// mutationConf.plugins = [];
+module.exports = mutationConf;
