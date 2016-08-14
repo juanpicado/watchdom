8367368eec9113b1758e13b6a677d7008a9823b2
Juan Carlos Picado <juanpicado19@gmail.com>
2016-08-14 15:50:20 +0200

    update babel webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
 var DefaultConfig = require('webpack-babel-conf');
-var mutationConf = DefaultConfig('./lib/browser.js', './watchdom.browser.min.js', 'domWatcher');
-// mutationConf.plugins = [];
+var mutationConf = DefaultConfig('./lib/browser.js', './dist', 'watchdom.browser.min.js', 'domWatcher');
+//mutationConf.plugins = [];
 module.exports = mutationConf;
